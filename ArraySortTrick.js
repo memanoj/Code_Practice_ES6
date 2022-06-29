61131529a343339b56eb16c1ae0e511f052ac6ca
Manoj Bhardwaj <manoj8679778910@gmail.com>
2022-06-29 17:58:10 +0530

    Create ArraySortTrick.js

---
+++ ArraySortTrick.js
@@ -0,0 +1,7 @@
+let numbers = [5, 13, 1, 44, 32, 15, 500]
+//Max value order
+numbers.sort((a, b) => a - b)
+console.info(numbers)
+//Min value order
+numbers.sort((a, b) => b - a)
+console.info(numbers)
